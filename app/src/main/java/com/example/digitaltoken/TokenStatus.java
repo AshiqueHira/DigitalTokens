7e5f9748c48eb359f10d709ab1a804ea0f4a867b
AshiqueHiraManzil <ashiquehm1@gmail.com>
2020-05-30 23:26:13 +0530

    token status

---
+++ app/src/main/java/com/example/digitaltoken/TokenStatus.java
@@ -0,0 +1,14 @@
+package com.example.digitaltoken;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TokenStatus extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_token_status);
+    }
+}
