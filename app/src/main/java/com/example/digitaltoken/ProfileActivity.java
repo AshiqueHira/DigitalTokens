d8f9be6591401e095f5c45bd2b080bc19ec4daea
AshiqueHiraManzil <ashiquehm1@gmail.com>
2020-07-22 22:17:08 +0530

    delete Account

--- app/src/main/java/com/example/digitaltoken/ProfileActivity.java
+++ app/src/main/java/com/example/digitaltoken/ProfileActivity.java
@@ -9,6 +9,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -16,10 +17,13 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -33,6 +37,7 @@ public class ProfileActivity extends AppCompatActivity {
     Toolbar toolbar;
 
     DatabaseReference usersDataReference;
+    DatabaseReference msgDataReference;
     FirebaseUser user;
     FirebaseAuth firebaseAuth;
     String userId = "";
@@ -41,6 +46,7 @@ public class ProfileActivity extends AppCompatActivity {
     String userEmail = "";
     String userPhone = "";
     String inputEmail;
+    String password = "";
 
     EditText userNameET;
     EditText userLocationET;
@@ -70,10 +76,14 @@ public class ProfileActivity extends AppCompatActivity {
         userEmailET = findViewById(R.id.userEmailEditText);
         userPhoneET = findViewById(R.id.userPhoneEditText);
 
+
         user = FirebaseAuth.getInstance().getCurrentUser();
+
+
         userId = user.getUid();
 
         usersDataReference = FirebaseDatabase.getInstance().getReference("Users");
+        msgDataReference = FirebaseDatabase.getInstance().getReference("Messages");
 
         usersDataReference.child(userId).addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
@@ -125,8 +135,8 @@ public class ProfileActivity extends AppCompatActivity {
 
         if (item.getItemId() == R.id.delete) {
 
-            FirebaseAuth.getInstance().signOut();
-            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            warningMethod();
+
             return true;
 
         }
@@ -181,6 +191,82 @@ public class ProfileActivity extends AppCompatActivity {
             }
         });
         forgotBuilder.show();
+
+    }
+
+    public void warningMethod() {
+
+        LayoutInflater inflater = getLayoutInflater();
+        final View dialogLayout = inflater.inflate(R.layout.delete_user_dialog, null);
+        final EditText passwordEditText = dialogLayout.findViewById(R.id.passwordEditText);
+        final TextView emailTextView = dialogLayout.findViewById(R.id.emailTextView);
+        emailTextView.setText(userEmail);
+        final AlertDialog deleteDialog = new AlertDialog.Builder(this)
+                .setView(dialogLayout)
+                .setPositiveButton("Confirm", null)
+                .setNegativeButton("cancel", null)
+                .create();
+
+        deleteDialog.setOnShowListener(new DialogInterface.OnShowListener() {
+            @Override
+            public void onShow(DialogInterface dialog) {
+                Button button = ((AlertDialog) deleteDialog).getButton(AlertDialog.BUTTON_POSITIVE);
+                button.setOnClickListener(new View.OnClickListener() {
+
+                    @Override
+                    public void onClick(View view) {
+                        password = passwordEditText.getText().toString();
+                        if (TextUtils.isEmpty(password)) {
+                            passwordEditText.setError("please enter your password");
+                        } else {
+                            AuthCredential credential = EmailAuthProvider.getCredential(userEmail, password);
+                            user.reauthenticate(credential)
+                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            if (task.isSuccessful()) {
+
+                                                user.delete()
+                                                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                            @Override
+                                                            public void onComplete(@NonNull Task<Void> task) {
+                                                                if (task.isSuccessful()) {
+                                                                    usersDataReference.child(userId).removeValue()
+                                                                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                                                @Override
+                                                                                public void onComplete(@NonNull Task<Void> task) {
+                                                                                    if (task.isSuccessful()) {
+                                                                                        msgDataReference.child(userId).removeValue()
+                                                                                                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                                                                    @Override
+                                                                                                    public void onComplete(@NonNull Task<Void> task) {
+                                                                                                        FirebaseAuth.getInstance().signOut();
+                                                                                                    }
+                                                                                                });
+                                                                                    } else {
+                                                                                        Toast.makeText(ProfileActivity.this, "Error ! " + task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                                                                                    }
+                                                                                }
+                                                                            });
+                                                                } else {
+                                                                    Toast.makeText(ProfileActivity.this, "Error ! " + task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                                                                }
+                                                            }
+                                                        });
+
+                                            } else {
+                                                Toast.makeText(ProfileActivity.this, "Error ! " + task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                                            }
+
+                                        }
+                                    });
+                        }
+                    }
+
+                });
+            }
+        });
+        deleteDialog.show();
 
     }
 }
